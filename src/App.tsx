860c46ad6d50423367f7775f6ce15be3b97288dc
hotdog1004 <hotdog87675210@gmail.com>
2023-07-25 23:33:19 +0900

    Feat : 라우터 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import React from "react";
-import Home from "pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "components/Header";
 import Footer from "components/Footer";
+import Result from "pages/Result";
+import Home from "pages/Home";
 
 const App = () => {
     return (
-        <>
+        <BrowserRouter>
             <Header />
-            <Home></Home>
+            <Routes>
+                <Route path="/" element={<Home />}></Route>
+                <Route path="/result" element={<Result />}></Route>
+            </Routes>
             <Footer />
-        </>
+        </BrowserRouter>
     )
 }
 
