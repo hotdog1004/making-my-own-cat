0c4961c2faaa66c4abf2ad5b09c9c305960926f5
hotdog1004 <hotdog87675210@gmail.com>
2023-07-25 19:14:15 +0900

    Feat : layout 구조 변경 Home, Result 페이지 추가

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import MainLayout from "layouts/MainLayout";
+
+const Home = () => {
+    return (
+        <MainLayout>
+            <div>메인이다</div>
+        </MainLayout>
+    );
+}
+
+export default Home;
