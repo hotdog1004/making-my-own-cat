b130e12eeef973280e8a4fc8eb7cc28e76f06c20
hotdog1004 <hotdog87675210@gmail.com>
2023-08-04 17:29:19 +0900

    Style : header 가운데 정렬 위해 css 수정

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <header className='bg-amber-300	text-center flex flex-col items-center font-semibold h-11'>
-            <h1> 나만의 고양이 만들기 </h1>
+        <header className='bg-amber-300	text-center align-middle  font-semibold self-center'>
+            <h1 className='py-2'> 나만의 고양이 만들기 </h1>
         </header>
     );
 }
