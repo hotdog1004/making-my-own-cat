4a9cde103653ce502d5e41f63ffaf30c759b5dec
hotdog1004 <hotdog87675210@gmail.com>
2023-08-16 21:19:15 +0900

    Style : css 수정

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -5,6 +5,6 @@ interface MainLayoutProps {
   children: ReactNode;
 }
 const MainLayout = ({ children }: MainLayoutProps) => {
-  return <main className="bg-white h-4/5 my-12">{children}</main>;
+  return <main className="bg-white my-12">{children}</main>;
 };
 export default MainLayout;
