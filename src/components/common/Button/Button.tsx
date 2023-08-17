5f8e38848b6a55917fbed2d349f6ad077597e49c
hotdog1004 <hotdog87675210@gmail.com>
2023-08-17 23:56:40 +0900

    Style : button css 적용

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -1,5 +1,30 @@
 import React from 'react';
 
+// --button-font-size: 1.25rem;
+// --button-padding: 16px 20px;
+// --button-radius: 12px;
+// width: 100%;
+// margin: 8px 0px 8px 0px;
+//   border: none;
+//   cursor: pointer;
+//   font-family: "Noto Sans KR", sans-serif;
+//   font-size: var(--button-font-size, 1rem);
+//   padding: var(--button-padding, 12px 16px);
+//   border-radius: var(--button-radius, 8px);
+//   color: var(--black);
+//   background: var(--button-bg-color, var(--main-color));
+
+//   &:active,
+//   &:hover,
+//   &:focus {
+//     background: var(--button-hover-bg-color, #bfa92a);
+//   }
+
+//   &:disabled {
+//     cursor: default;
+//     opacity: 0.5;
+//     background: var(--button-bg-color, #4d4d4);
+//   }
 interface ButtonProps {
   onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
   children?: React.ReactNode;
@@ -7,7 +32,11 @@ interface ButtonProps {
 }
 const Button = ({ onClick, children, disabled }: ButtonProps) => {
   return (
-    <button disabled={disabled} onClick={onClick}>
+    <button
+      className="rounded-lg m-2 bg-amber-300 cursor-pointer text-xl py-4 px-5 w-full"
+      disabled={disabled}
+      onClick={onClick}
+    >
       {children}
     </button>
   );
