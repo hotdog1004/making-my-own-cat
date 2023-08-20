e9232318dad4b61b8f952a395a07114ab99a9c06
hotdog1004 <hotdog87675210@gmail.com>
2023-08-20 16:46:12 +0900

    Style : 메인레이아웃 css 수정

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,10 +1,9 @@
-import { ReactNode } from 'react';
 import React from 'react';
 
 interface MainLayoutProps {
-  children: ReactNode;
+  children: React.ReactNode;
 }
 const MainLayout = ({ children }: MainLayoutProps) => {
-  return <main className="bg-white my-12">{children}</main>;
+  return <main className="bg-white mt-2.5 h-[92%]">{children}</main>;
 };
 export default MainLayout;
