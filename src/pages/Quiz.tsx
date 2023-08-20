383e0c405ae3aa9eb50061ae2691344886acc1f2
hotdog1004 <hotdog87675210@gmail.com>
2023-08-20 19:02:57 +0900

    Feat : 질문 페이지 추가

---
+++ src/pages/Quiz.tsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import contents from './contents/questions';
+import Button from 'components/common/Button/Button';
+const Quiz = () => {
+  const [questionNum, setQuestionNum] = useState(0);
+
+  const onClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    setQuestionNum(questionNum + 1);
+  };
+  return (
+    <>
+      <div>
+        <h1>{contents[questionNum].question} </h1>
+        {contents[questionNum].answers.map((answer, i) => (
+          <h2 key={i}>{answer}</h2>
+        ))}
+      </div>
+      <div className="flex">
+        <Button onClick={(e) => onClick(e)} disabled={false}>
+          선택완료
+        </Button>
+      </div>
+    </>
+  );
+};
+
+export default Quiz;
