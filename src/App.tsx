23f42a6f4296acd88f1ea76e1d26d40b25ffcd9c
hotdog1004 <hotdog87675210@gmail.com>
2023-08-20 16:47:11 +0900

    Style : footer 주석처리

--- src/App.tsx
+++ src/App.tsx
@@ -9,14 +9,14 @@ const App = () => {
   return (
     <div className="flex relative z-[1] overflow-scroll mx-auto my-0 max-w-sm min-h-[667px] h-full bg-white">
       <div className="sm:max-w-xl w-full ">
+        <Header />
         <BrowserRouter>
-          <Header />
           <Routes>
             <Route path="/" element={<Home />}></Route>
             <Route path="/result" element={<Result />}></Route>
           </Routes>
-          <Footer />
         </BrowserRouter>
+        {/* <Footer /> */}
       </div>
     </div>
   );
