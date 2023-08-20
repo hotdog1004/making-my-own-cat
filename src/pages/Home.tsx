5f2287dd12a9494bea457856882491cb9a72d6d1
hotdog1004 <hotdog87675210@gmail.com>
2023-08-20 16:46:32 +0900

    Feat : 메인에 버튼 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import MainLayout from 'layouts/MainLayout';
+import Button from 'components/common/Button/Button';
 
 const Home = () => {
+  const onClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    console.log(e);
+  };
   return (
     <MainLayout>
-      <div>메인이다</div>
+      <div className="h-5/6">
+        <h2>어느날 우연히 냥줍을 하게 된 당신!</h2>
+        <h2>나의 고양이는 어떤 고양이일까?</h2>
+        <h2>(=◉ᆽ◉=) </h2>
+      </div>
+      <div className="flex bottom-0">
+        <Button onClick={(e) => onClick(e)} disabled={false}>
+          내 고양이 만나러가기
+        </Button>
+      </div>
     </MainLayout>
   );
 };
