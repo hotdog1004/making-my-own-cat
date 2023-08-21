0c5a8f68e943c858a8f714807cc9cccbbaf620c9
hotdog1004 <hotdog87675210@gmail.com>
2023-08-22 00:16:29 +0900

    Feat : Progressbar 적용

--- src/pages/Quiz.tsx
+++ src/pages/Quiz.tsx
@@ -1,15 +1,18 @@
 import React, { useState } from 'react';
 import contents from './contents/questions';
 import Button from 'components/Button';
+import ProgressBar from 'components/ProgressBar';
 const Quiz = () => {
   const [questionNum, setQuestionNum] = useState(0);
-
+  const [completed, setCompleted] = useState(1);
   const onClick = (e: React.MouseEvent<HTMLButtonElement>) => {
     setQuestionNum(questionNum + 1);
+    setCompleted(completed + 1);
   };
   return (
     <>
       <div>
+        <ProgressBar completed={completed} />
         <h1>{contents[questionNum].question} </h1>
         {contents[questionNum].answers.map((answer, i) => (
           <h2 key={i}>{answer}</h2>
