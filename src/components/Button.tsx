a78b50da3813f73d971ad967f950f1d9b3b0740c
hotdog1004 <hotdog87675210@gmail.com>
2023-08-21 22:47:44 +0900

    Comment : 주석 삭제

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,30 +1,5 @@
 import React from 'react';
 
-// --button-font-size: 1.25rem;
-// --button-padding: 16px 20px;
-// --button-radius: 12px;
-// width: 100%;
-// margin: 8px 0px 8px 0px;
-//   border: none;
-//   cursor: pointer;
-//   font-family: "Noto Sans KR", sans-serif;
-//   font-size: var(--button-font-size, 1rem);
-//   padding: var(--button-padding, 12px 16px);
-//   border-radius: var(--button-radius, 8px);
-//   color: var(--black);
-//   background: var(--button-bg-color, var(--main-color));
-
-//   &:active,
-//   &:hover,
-//   &:focus {
-//     background: var(--button-hover-bg-color, #bfa92a);
-//   }
-
-//   &:disabled {
-//     cursor: default;
-//     opacity: 0.5;
-//     background: var(--button-bg-color, #4d4d4);
-//   }
 interface ButtonProps {
   onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
   children?: React.ReactNode;
