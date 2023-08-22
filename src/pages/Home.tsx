a61734a5da4081f9c99c20a8331fd05a93c2e281
hotdog1004 <hotdog87675210@gmail.com>
2023-08-23 00:39:36 +0900

    Chore : 공백추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import MainLayout from 'layouts/MainLayout';
 import Button from 'components/Button';
 import { useNavigate } from 'react-router-dom';
+
 const Home = () => {
   const movePage = useNavigate();
   const onClick = () => {
