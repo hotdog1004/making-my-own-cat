263b4f012db1bcb479eaf74c1c14c839ec3126ad
hotdog1004 <hotdog87675210@gmail.com>
2023-08-23 00:40:07 +0900

    Style : quiz 페이지 응답부분 css 수정

--- src/pages/Quiz.tsx
+++ src/pages/Quiz.tsx
@@ -11,13 +11,23 @@ const Quiz = () => {
   };
   return (
     <>
-      <div>
+      <div className="mb-16">
         <ProgressBar completed={completed} />
-        <h1>{contents[questionNum].question} </h1>
-        {contents[questionNum].answers.map((answer, i) => (
-          <h2 key={i}>{answer}</h2>
-        ))}
+        <div className="text-2xl mx-2 mt-16 mb-32 text-center">
+          {contents[questionNum].question}
+        </div>
+        <div className="grid grid-cols-2 justify-center place-items-center	">
+          {contents[questionNum].answers.map((answer, i) => (
+            <div
+              className="flex items-center relative justify-center border-2 border-solid border-slate-300 m-3 rounded-md w-32 h-24"
+              key={i}
+            >
+              {answer}
+            </div>
+          ))}
+        </div>
       </div>
+
       <div className="flex">
         <Button onClick={(e) => onClick(e)} disabled={false}>
           선택완료
