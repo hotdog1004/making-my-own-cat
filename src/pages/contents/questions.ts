3d8655d3a7175b13b408da67e3c1d89f37649180
hotdog1004 <hotdog87675210@gmail.com>
2023-08-24 23:05:09 +0900

    Chore : 질문 내용 수정

--- src/pages/contents/questions.ts
+++ src/pages/contents/questions.ts
@@ -5,23 +5,23 @@ interface QuestionType {
 
 const questions: QuestionType[] = [
   {
-    question: '색깔을 골라주세요',
+    question: '색상을 선택하세요',
     answers: ['답변1', '답변2', '답변3', '답변4'],
   },
   {
-    question: '양말 모양을 골라주세요',
+    question: '양말 모양을 선택하세요',
     answers: ['답변1', '답변2', '답변3', '답변4'],
   },
   {
-    question: '코 색깔을 골라주세요',
+    question: '코 색깔을 선택하세요',
     answers: ['답변1', '답변2'],
   },
   {
-    question: '젤리를 골라주세요',
+    question: '젤리를 선택하세요',
     answers: ['답변1', '답변2', '답변3'],
   },
   {
-    question: '보석을 골라주세요',
+    question: '보석을 선택하세요',
     answers: ['답변1', '답변2', '답변3', '답변4'],
   },
 ];
