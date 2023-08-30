d4a5f5e9c1d645dd9038a2385ede309ab894c22e
hotdog1004 <hotdog87675210@gmail.com>
2023-08-30 12:23:10 +0900

    Refactor : 질문 리스트에 id추가

--- src/pages/contents/questions.ts
+++ src/pages/contents/questions.ts
@@ -1,4 +1,5 @@
 interface QuestionType {
+  id: string;
   question: string;
   answers: string[];
   type: string;
@@ -6,31 +7,37 @@ interface QuestionType {
 
 const questions: QuestionType[] = [
   {
+    id: 'color',
     question: '색상을 선택하세요',
     answers: ['답변1', '답변2', '답변3', '답변4'],
     type: 'check',
   },
   {
+    id: 'socks',
     question: '양말 모양을 선택하세요',
     answers: ['답변1', '답변2', '답변3', '답변4'],
     type: 'radio',
   },
   {
+    id: 'noseColor',
     question: '코 색깔을 선택하세요',
     answers: ['답변1', '답변2'],
     type: 'radio',
   },
   {
+    id: 'jelly',
     question: '젤리를 선택하세요',
     answers: ['답변1', '답변2', '답변3'],
     type: 'radio',
   },
   {
+    id: 'jewel',
     question: '보석을 선택하세요',
     answers: ['답변1', '답변2', '답변3', '답변4'],
     type: 'radio',
   },
   {
+    id: 'name',
     question: '이름을 입력하세요.',
     answers: ['name'],
     type: 'input',
