a20da0125af5a8a7d8d4657e0f6f02d54d3a93c8
hotdog1004 <hotdog87675210@gmail.com>
2023-09-01 00:17:32 +0900

    Refactor : img 주소 추가 위해 수정

--- src/pages/contents/questions.ts
+++ src/pages/contents/questions.ts
@@ -1,45 +1,74 @@
+import ColorImg from '../../assets/answers/hair_color_yellow.svg';
+
 interface QuestionType {
   id: string;
   question: string;
-  answers: string[];
+  answers: answerType[];
   type: string;
+}
+
+interface answerType {
+  id: string;
+  img: string;
 }
 
 const questions: QuestionType[] = [
   {
     id: 'color',
     question: '색상을 선택하세요',
-    answers: ['black', 'white', 'yellow', 'brown'],
+    answers: [
+      { id: 'black', img: ColorImg },
+      { id: 'white', img: ColorImg },
+      { id: 'yellow', img: ColorImg },
+      { id: 'brown', img: ColorImg },
+    ],
     type: 'check',
   },
   {
     id: 'socks',
     question: '양말 모양을 선택하세요',
-    answers: ['답변1', '답변2', '답변3', '답변4'],
+    answers: [
+      { id: 'black', img: '' },
+      { id: 'white', img: '' },
+      { id: 'yellow', img: '' },
+      { id: 'brown', img: '' },
+    ],
     type: 'radio',
   },
   {
     id: 'noseColor',
     question: '코 색깔을 선택하세요',
-    answers: ['답변1', '답변2'],
+    answers: [
+      { id: 'black', img: '' },
+      { id: 'white', img: '' },
+    ],
     type: 'radio',
   },
   {
     id: 'jelly',
     question: '젤리를 선택하세요',
-    answers: ['답변1', '답변2', '답변3'],
+    answers: [
+      { id: 'black', img: '' },
+      { id: 'white', img: '' },
+      { id: 'yellow', img: '' },
+    ],
     type: 'radio',
   },
   {
     id: 'jewel',
     question: '보석을 선택하세요',
-    answers: ['답변1', '답변2', '답변3', '답변4'],
+    answers: [
+      { id: 'black', img: '' },
+      { id: 'white', img: '' },
+      { id: 'yellow', img: '' },
+      { id: 'brown', img: '' },
+    ],
     type: 'radio',
   },
   {
     id: 'name',
     question: '이름을 입력하세요.',
-    answers: ['name'],
+    answers: [{ id: 'name', img: '' }],
     type: 'input',
   },
 ];
