bd98cf72922315410d6e8fe3edaf4ad9bfcece48
hotdog1004 <hotdog87675210@gmail.com>
2023-09-02 01:11:52 +0900

    Feat : 라디오 컴포넌트 추가

---
+++ src/components/Radio.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+interface RadioProps {
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  id: string;
+  checked: boolean;
+  img: string;
+}
+
+const Radio = ({ id, checked, onChange, img }: RadioProps) => {
+  return (
+    <div
+      className={`${
+        checked ? 'border-slate-950' : 'border-slate-300'
+      } flex items-center justify-center border-2 border-solid  m-3 rounded-md w-40 h-32`}
+    >
+      <input
+        id={id}
+        type="radio"
+        checked={checked}
+        onChange={onChange}
+        className="hidden"
+      />
+      <label htmlFor={id} className="cursor-pointer checked:bg-gray-950">
+        <img src={img} className="w-32 h-32" />
+      </label>
+    </div>
+  );
+};
+
+export default Radio;
