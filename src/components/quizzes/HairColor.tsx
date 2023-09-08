747f3255eeed4d0a6d11973ec202f0acdaf1a1e9
hotdog1004 <hotdog87675210@gmail.com>
2023-09-08 13:45:07 +0900

    Style : 버튼, 질문내용 추가, 버튼 이벤트 props에 추가

--- src/components/quizzes/HairColor.tsx
+++ src/components/quizzes/HairColor.tsx
@@ -1,8 +1,10 @@
 import React, { Fragment, useState } from 'react';
 import contents from '../../pages/contents/questions';
 import Checkbox from 'components/Checkbox';
+import Button from 'components/Button';
+import { FunnelProp } from 'pages/QuizFunnel';
 
-const HairColor = () => {
+const HairColor = ({ onNext }: FunnelProp) => {
   const [checkedItems, setCheckedItems] = useState(new Set());
 
   const checkedItemsHandler = (id: string, isChecked: boolean) => {
@@ -21,18 +23,28 @@ const HairColor = () => {
     checkedItemsHandler(e.target.id, e.target.checked);
   };
   return (
-    <div className="flex flex-wrap justify-center w-full">
-      {contents[0].answers.map((answer) => (
-        <Fragment key={answer.id}>
-          <Checkbox
-            id={answer.id}
-            checked={checkedItems.has(answer.id)}
-            img={answer.img}
-            onChange={(e) => checkHandler(e)}
-          />
-        </Fragment>
-      ))}
-    </div>
+    <>
+      <div className="text-2xl mx-2 mt-16 text-center">
+        {contents[0].content}
+      </div>
+      <div className="flex flex-wrap justify-center w-full">
+        {contents[0].answers.map((answer) => (
+          <Fragment key={answer.id}>
+            <Checkbox
+              id={answer.id}
+              checked={checkedItems.has(answer.id)}
+              img={answer.img}
+              onChange={(e) => checkHandler(e)}
+            />
+          </Fragment>
+        ))}
+      </div>
+      <div className="flex absolute w-full max-w-sm bottom-0">
+        <Button onClick={(e) => onNext(e)} disabled={false}>
+          완료
+        </Button>
+      </div>
+    </>
   );
 };
 export default HairColor;
