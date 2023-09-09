a80b4eb45fffcd9d062224cf18e76feb518a2879
hotdog1004 <hotdog87675210@gmail.com>
2023-09-09 17:11:23 +0900

    Feat : 추가한 컴포넌트들 적용

--- src/pages/QuizFunnel.tsx
+++ src/pages/QuizFunnel.tsx
@@ -1,17 +1,31 @@
+import ProgressBar from 'components/ProgressBar';
 import HairColor from 'components/quizzes/HairColor';
+import Jelly from 'components/quizzes/Jelly';
+import Jewel from 'components/quizzes/Jewel';
+import Name from 'components/quizzes/Name';
+import NoseColor from 'components/quizzes/NoseColor';
 import Socks from 'components/quizzes/Socks';
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export interface FunnelProp {
   onNext: (e: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
 const QuizFunnel = () => {
-  const [step, setStep] = useState<'HairColor' | 'Socks'>('HairColor');
+  const movePage = useNavigate();
+  const [step, setStep] = useState<
+    'HairColor' | 'Socks' | 'NoseColor' | 'Jelly' | 'Jewel' | 'Name'
+  >('HairColor');
   return (
     <>
+      <ProgressBar completed={1} />
       {step === 'HairColor' && <HairColor onNext={() => setStep('Socks')} />}
-      {step === 'Socks' && <Socks onNext={() => setStep('Socks')} />}
+      {step === 'Socks' && <Socks onNext={() => setStep('NoseColor')} />}
+      {step === 'NoseColor' && <NoseColor onNext={() => setStep('Jelly')} />}
+      {step === 'Jelly' && <Jelly onNext={() => setStep('Jewel')} />}
+      {step === 'Jewel' && <Jewel onNext={() => setStep('Name')} />}
+      {step === 'Name' && <Name onNext={() => movePage('/result')} />}
     </>
   );
 };
