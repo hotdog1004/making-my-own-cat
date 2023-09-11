f3ae7258f02be508f94f2cdb82d0c20069045991
hotdog1004 <hotdog87675210@gmail.com>
2023-09-11 22:37:35 +0900

    Feat : 선택값 관리위해 useState 변수 추가

--- src/components/quizzes/Jelly.tsx
+++ src/components/quizzes/Jelly.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from 'components/Button';
 import RadioGroup from 'components/RadioGroup';
 import contents from '../../pages/contents/questions';
 import { FunnelProp } from 'pages/QuizFunnel';
 
 const Jelly = ({ onNext }: FunnelProp) => {
+  const [selectedValue, setSelectedValue] = useState('');
   return (
     <>
-      <RadioGroup question={contents[3]} />
+      <RadioGroup
+        question={contents[3]}
+        onChange={(data) => {
+          setSelectedValue(data);
+        }}
+      />
       <div className="flex absolute w-full max-w-sm bottom-0">
-        <Button onClick={(e) => onNext(e)} disabled={false}>
+        <Button onClick={(e) => onNext(e, selectedValue)} disabled={false}>
           완료
         </Button>
       </div>
