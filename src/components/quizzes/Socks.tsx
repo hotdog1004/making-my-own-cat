e550a42750ef73fbacfb1626017c6ad787f02724
hotdog1004 <hotdog87675210@gmail.com>
2023-09-11 22:37:58 +0900

    Feat : 선택값 관리위해 useState 변수 추가

--- src/components/quizzes/Socks.tsx
+++ src/components/quizzes/Socks.tsx
@@ -4,11 +4,17 @@ import RadioGroup from 'components/RadioGroup';
 import Button from 'components/Button';
 import { FunnelProp } from 'pages/QuizFunnel';
 const Socks = ({ onNext }: FunnelProp) => {
+  const [selectedValue, setSelectedValue] = useState('');
   return (
     <>
-      <RadioGroup question={contents[1]} />
+      <RadioGroup
+        question={contents[1]}
+        onChange={(data) => {
+          setSelectedValue(data);
+        }}
+      />
       <div className="flex absolute w-full max-w-sm bottom-0">
-        <Button onClick={(e) => onNext(e)} disabled={false}>
+        <Button onClick={(e) => onNext(e, selectedValue)} disabled={false}>
           완료
         </Button>
       </div>
