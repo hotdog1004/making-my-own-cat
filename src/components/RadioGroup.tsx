ed1a20999c4fe604ed5ccfd37e8b7f10758f36d3
hotdog1004 <hotdog87675210@gmail.com>
2023-09-11 21:45:49 +0900

    Faet : RadioGroup Props에 onChange 추가

--- src/components/RadioGroup.tsx
+++ src/components/RadioGroup.tsx
@@ -4,9 +4,14 @@ import { QuestionType } from 'pages/contents/questions';
 
 interface RadioGroupProps {
   question: QuestionType;
+  onChange: (checkedValue: string) => void;
 }
-const RadioGroup = ({ question }: RadioGroupProps) => {
+const RadioGroup = ({ question, onChange }: RadioGroupProps) => {
   const [selectedValue, setSelectedValue] = useState('');
+  const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSelectedValue(e.target.id);
+    onChange(e.target.id);
+  };
   return (
     <>
       <div className="text-2xl mx-2 mt-16 text-center">{question.content}</div>
@@ -17,7 +22,7 @@ const RadioGroup = ({ question }: RadioGroupProps) => {
               id={answer.id}
               img={answer.img}
               name={question.id}
-              onChange={() => setSelectedValue(answer.id)}
+              onChange={(e) => changeHandler(e)}
               checked={selectedValue == answer.id}
             />
           </Fragment>
