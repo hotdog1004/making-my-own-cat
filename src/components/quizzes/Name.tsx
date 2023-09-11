c34512b2de91d895029351c4e47eeac952745c1f
hotdog1004 <hotdog87675210@gmail.com>
2023-09-11 22:37:46 +0900

    Feat : 선택값 관리위해 useState 변수 추가

--- src/components/quizzes/Name.tsx
+++ src/components/quizzes/Name.tsx
@@ -11,17 +11,15 @@ const Name = ({ onNext }: FunnelProp) => {
     <>
       <div className="text-2xl mx-2 mt-16 text-center">이름을 입력하세요.</div>
       <div className="flex flex-wrap justify-center w-full">
-        {/* <div className="flex"> */}
         <input
           type="text"
           value={catName}
           onChange={changeInput}
           className="w-full border-solid border-2  rounded border-gray-400 p-3 m-4"
         />
-        {/* </div> */}
       </div>
       <div className="flex absolute w-full max-w-sm bottom-0">
-        <Button onClick={(e) => onNext(e)} disabled={false}>
+        <Button onClick={(e) => onNext(e, catName)} disabled={false}>
           내 고양이 만나러가기
         </Button>
       </div>
