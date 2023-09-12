838f8870071446c1b4adeccaefef18dba8a1abbe
hotdog1004 <hotdog87675210@gmail.com>
2023-09-12 19:29:54 +0900

    Feat : Button Props에 color 추가

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,11 +4,20 @@ interface ButtonProps {
   onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
   children?: React.ReactNode;
   disabled?: boolean;
+  color?: string;
 }
-const Button = ({ onClick, children, disabled }: ButtonProps) => {
+const colors = {
+  yellow: 'bg-amber-300 hover:bg-[#EBC448]',
+  gray: 'bg-gray-200 hover:bg-gray-300',
+};
+const Button = ({ onClick, children, disabled, color }: ButtonProps) => {
+  const buttonColor = () => {
+    if (color === undefined) return `${colors['yellow']}`;
+    else return `${colors['gray']}`;
+  };
   return (
     <button
-      className="rounded-lg m-2 bg-amber-300 cursor-pointer font-bold  py-4 px-5 w-full hover:bg-[#EBC448] disabled:cursor-default disabled:opacity-50 disabled:bg-slate-300"
+      className={`rounded-lg m-2 bg-amber-300 cursor-pointer font-bold  py-4 px-5 w-full disabled:cursor-default disabled:opacity-50 disabled:bg-slate-300 ${buttonColor()}`}
       disabled={disabled}
       onClick={onClick}
     >
