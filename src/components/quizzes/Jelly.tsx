acc3c0927dbfecd3a405f409be80be0b0d95b596
hotdog1004 <hotdog87675210@gmail.com>
2023-09-12 13:00:23 +0900

    Refactor : selectedValue state기본값추가

--- src/components/quizzes/Jelly.tsx
+++ src/components/quizzes/Jelly.tsx
@@ -5,7 +5,7 @@ import contents from '../../pages/contents/questions';
 import { FunnelProp } from 'pages/QuizFunnel';
 
 const Jelly = ({ onNext }: FunnelProp) => {
-  const [selectedValue, setSelectedValue] = useState('');
+  const [selectedValue, setSelectedValue] = useState(contents[3].answers[0].id);
   return (
     <>
       <RadioGroup
