6d5cb2ad498094ac699115fbc2bac3a18eefcd04
hotdog1004 <hotdog87675210@gmail.com>
2023-09-14 23:45:43 +0900

    Faet : 결과페이지 버튼 추가

--- src/pages/Result.tsx
+++ src/pages/Result.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import MainLayout from 'layouts/MainLayout';
+import Button from 'components/Button';
 
 const Result = () => {
   return (
     <MainLayout>
-      <div>결과페이지다</div>
+      <div className="mt-16 text-center">결과페이지다</div>
+      <div className="flex absolute w-full max-w-sm bottom-0">
+        <Button
+          onClick={(e) => {
+            console.log('테스트 다시하기');
+          }}
+          disabled={false}
+          color="gray"
+        >
+          테스트 다시 하기
+        </Button>
+      </div>
     </MainLayout>
   );
 };
