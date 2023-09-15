6cd790b301b558250c37582e80709f421d26bea4
hotdog1004 <hotdog87675210@gmail.com>
2023-09-16 02:21:20 +0900

    Refactor : quiz 페이지로 이동하기 추가

--- src/pages/Result.tsx
+++ src/pages/Result.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import MainLayout from 'layouts/MainLayout';
 import Button from 'components/Button';
+import { useNavigate } from 'react-router-dom';
 
 const Result = () => {
+  const movePage = useNavigate();
+  const onClick = () => {
+    movePage('/quiz');
+  };
   return (
     <MainLayout>
       <div className="mt-16 text-center">결과페이지다</div>
       <div className="flex absolute w-full max-w-sm bottom-0">
         <Button
           onClick={(e) => {
-            console.log('테스트 다시하기');
+            onClick();
           }}
           disabled={false}
           color="gray"
