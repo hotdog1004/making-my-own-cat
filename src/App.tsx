0dd5cca9834880b7960419f9d8dae14e55247484
hotdog1004 <hotdog87675210@gmail.com>
2023-09-17 22:36:10 +0900

    Feat : QuizPage route 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from 'components/Header';
-import Footer from 'components/Footer';
 import Result from 'pages/Result';
 import Home from 'pages/Home';
-import Quiz from 'pages/Quiz';
-import QuizFunnel from 'pages/QuizFunnel';
+import QuizPage from 'pages/QuizPage';
 const App = () => {
   return (
     <div className="flex relative z-[1] overflow-scroll mx-auto my-0 max-w-sm min-h-[667px] h-full bg-white">
       <div className="sm:max-w-xl w-full ">
-        <Header />
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Home />}></Route>
             <Route path="/result" element={<Result />}></Route>
-            {/* <Route path="/quiz" element={<Quiz />}></Route> */}
-            <Route path="/quiz" element={<QuizFunnel />}></Route>
+            <Route path="/quiz" element={<QuizPage />}></Route>
           </Routes>
         </BrowserRouter>
-        {/* <Footer /> */}
       </div>
     </div>
   );
