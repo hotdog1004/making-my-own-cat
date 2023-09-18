dd3b08cb2079c91789afcbdb4c2d429724f10939
hotdog1004 <hotdog87675210@gmail.com>
2023-09-19 00:58:24 +0900

    Refactor : 프로그래스바 props에 따라 나타내도록 수정

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
 
-type widthType = Record<number, string>;
-const width: widthType = {
-  1: 'w-[11%]',
-  2: 'w-[22%]',
-  3: 'w-[33%]',
-  4: 'w-[44%]',
-  5: 'w-[55%]',
-  6: 'w-[66%]',
-  7: 'w-[77%]',
-  8: 'w-[88%]',
-  9: 'w-[100%]',
-};
-const ProgressBar = (props: { completed: number }) => {
+const ProgressBar = (props: { totalNumber: number; currentNumber: number }) => {
+  const getCompleted = (totalNumber: number, currentNumber: number) => {
+    return 100 * (currentNumber / totalNumber);
+  };
   return (
     <div className="h-3 w-96 bg-slate-200 rounded-lg mt-14 mb-16 ">
       <div
-        className={`h-full bg-amber-300 rounded-lg ${
-          width[props.completed]
-        } transition-all duration-1000 ease-out delay-75`}
+        style={{
+          width: `${getCompleted(props.totalNumber, props.currentNumber)}%`,
+        }}
+        className="h-full bg-amber-300 rounded-lg transition-all duration-1000 ease-out delay-75"
       ></div>
     </div>
   );
