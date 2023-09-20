654b26925cc272c5fc750bc810633145b0f18d51
hotdog1004 <hotdog87675210@gmail.com>
2023-09-20 23:10:13 +0900

    Feat : 결과 리스트 추가

---
+++ src/pages/contents/resultList.ts
@@ -0,0 +1,107 @@
+interface Result {
+  type: string;
+  content: string;
+  catType: string;
+  img: string;
+}
+
+const resultList: Result[] = [
+  {
+    type: 'INTJ',
+    content: '',
+    catType: '',
+    img: '',
+  },
+  {
+    type: 'INTP',
+    content: '',
+    catType: '',
+    img: '',
+  },
+  {
+    type: 'ENTJ',
+    content: '',
+    catType: '',
+    img: '',
+  },
+  {
+    type: 'ENTP',
+    content: '',
+    catType: '',
+    img: '',
+  },
+  {
+    type: 'INFJ',
+    content: '',
+    catType: '',
+    img: '',
+  },
+  {
+    type: 'INFP',
+    content: '',
+    catType: '',
+    img: '',
+  },
+  {
+    type: 'ENFJ',
+    content: '',
+    catType: '',
+    img: '',
+  },
+  {
+    type: 'ENFP',
+    content: '',
+    catType: '',
+    img: '',
+  },
+  {
+    type: 'ISTJ',
+    content: '',
+    catType: '',
+    img: '',
+  },
+  {
+    type: 'ISFJ',
+    content: '',
+    catType: '',
+    img: '',
+  },
+  {
+    type: 'ESTJ',
+    content: '',
+    catType: '',
+    img: '',
+  },
+  {
+    type: 'ESFJ',
+    content: '',
+    catType: '',
+    img: '',
+  },
+  {
+    type: 'ISTP',
+    content: '',
+    catType: '',
+    img: '',
+  },
+  {
+    type: 'ISFP',
+    content: '',
+    catType: '',
+    img: '',
+  },
+  {
+    type: 'ESTP',
+    content: '',
+    catType: '',
+    img: '',
+  },
+  {
+    type: 'ESFP',
+    content: '',
+    catType: '',
+    img: '',
+  },
+];
+
+export default resultList;
