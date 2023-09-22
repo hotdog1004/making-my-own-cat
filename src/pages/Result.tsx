51e46acd82f79e1af215195a35d523ae62ad5855
hotdog1004 <hotdog87675210@gmail.com>
2023-09-22 14:57:15 +0900

    Feat : searchParams 추가 및 적용

--- src/pages/Result.tsx
+++ src/pages/Result.tsx
@@ -1,29 +1,53 @@
 import React from 'react';
 import MainLayout from 'layouts/MainLayout';
 import Button from 'components/Button';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import resultList from './contents/resultList';
 
 const Result = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const result = Number(searchParams.get('type') ?? '-1');
   const movePage = useNavigate();
   const onClick = () => {
     movePage('/quiz');
   };
-  return (
-    <MainLayout>
-      <div className="mt-16 text-center">결과페이지다</div>
-      <div className="flex absolute w-full max-w-sm bottom-0">
-        <Button
-          onClick={(e) => {
-            onClick();
-          }}
-          disabled={false}
-          color="gray"
-        >
-          테스트 다시 하기
-        </Button>
-      </div>
-    </MainLayout>
-  );
+
+  if (result >= 0 && result <= resultList.length) {
+    return (
+      <MainLayout>
+        <div className="mt-16 text-center">결과페이지다{result}</div>
+        <h3>{resultList[result].type}</h3>
+        <div className="flex absolute w-full max-w-sm bottom-0">
+          <Button
+            onClick={(e) => {
+              onClick();
+            }}
+            disabled={false}
+            color="gray"
+          >
+            테스트 다시 하기
+          </Button>
+        </div>
+      </MainLayout>
+    );
+  } else {
+    return (
+      <MainLayout>
+        <div className="mt-16 text-center">잘못된 결과</div>
+        <div className="flex absolute w-full max-w-sm bottom-0">
+          <Button
+            onClick={(e) => {
+              onClick();
+            }}
+            disabled={false}
+            color="gray"
+          >
+            테스트 다시 하기
+          </Button>
+        </div>
+      </MainLayout>
+    );
+  }
 };
 
 export default Result;
