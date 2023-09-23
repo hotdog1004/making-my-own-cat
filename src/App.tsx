d368f2462eb3be21cf8032527c5372fc5a5d9904
hotdog1004 <hotdog87675210@gmail.com>
2023-09-23 23:41:07 +0900

    Feat : 에러페이지 route 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Result from 'pages/Result';
 import Home from 'pages/Home';
 import QuizPage from 'pages/QuizPage';
+import ErrorPage from 'pages/ErrorPage';
 const App = () => {
   return (
     <div className="flex relative z-[1] overflow-scroll mx-auto my-0 max-w-sm min-h-[667px] h-full bg-white">
@@ -12,6 +13,7 @@ const App = () => {
             <Route path="/" element={<Home />}></Route>
             <Route path="/result" element={<Result />}></Route>
             <Route path="/quiz" element={<QuizPage />}></Route>
+            <Route path="/*" element={<ErrorPage />}></Route>
           </Routes>
         </BrowserRouter>
       </div>
