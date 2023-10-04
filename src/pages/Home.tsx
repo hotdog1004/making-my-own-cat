e47c1b9c1d07b791e266ae2915fc619814ec221a
hotdog1004 <hotdog87675210@gmail.com>
2023-10-04 23:32:28 +0900

    Style : Home 페이지 css 수정

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -16,12 +16,12 @@ const Home = () => {
   }, [window.Kakao]);
   return (
     <MainLayout>
-      <div className="mt-16 text-center">
+      <div className="flex flex-col items-center	justify-center h-full">
         <h2>어느날 우연히 냥줍을 하게 된 당신!</h2>
-        <h2>나의 고양이는 어떤 고양이일까?</h2>
+        <h2>나와 어울리는 고양이는 어떤 고양이일까?</h2>
         <h2>(=◉ᆽ◉=) </h2>
       </div>
-      <div className="flex absolute w-full max-w-sm bottom-0">
+      <div className="flex absolute w-full max-w-sm bottom-0 pb-8">
         <Button onClick={() => onClick()} disabled={false} type="move">
           내 고양이 만나러가기
         </Button>
