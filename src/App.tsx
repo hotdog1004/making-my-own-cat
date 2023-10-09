a8c79f4edb78e4b342be227b356b42f99b061aa4
hotdog1004 <hotdog87675210@gmail.com>
2023-10-09 18:43:53 +0900

    Fix : 카카오 sdk init 위치 변경
    공유하기에서 접근시 카카오 sdk init 안 되는 문제 해결

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Result from 'pages/Result';
 import Home from 'pages/Home';
 import QuizPage from 'pages/QuizPage';
 import ErrorPage from 'pages/ErrorPage';
 const App = () => {
+  useEffect(() => {
+    if (!window.Kakao.isInitialized()) {
+      window.Kakao.init(process.env.REACT_APP_KAKAO);
+    }
+  }, [window.Kakao]);
+
   return (
     <div className="flex relative z-[1] overflow-y-auto  mx-auto my-0 max-w-sm min-h-[667px] h-full">
       <div className="sm:max-w-xl w-full ">
