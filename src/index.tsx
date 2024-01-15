f389c01e79665b42b5712cdc160ff71464c243ab
hotdog1004 <hotdog87675210@gmail.com>
2024-01-15 18:36:50 +0900

    Feat : BrowserRouter 추가
    useLocation 사용위해 App.tsx에 위치하던 BrowserRouter를 상위 컴포넌트로 이동

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from 'App';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <div className="flex relative z-[1] overflow-y-auto  mx-auto my-0 max-w-sm min-h-[667px] h-full">
+      <div className="sm:max-w-xl w-full ">
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </div>
+    </div>
   </React.StrictMode>,
 );
