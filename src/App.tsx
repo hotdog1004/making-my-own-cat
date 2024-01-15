7b4bdc839b32173c5c7939d28ac012636a01c204
hotdog1004 <hotdog87675210@gmail.com>
2024-01-15 18:35:23 +0900

    Feat : URL 변경 감지 기능 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,26 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Result from 'pages/Result';
 import Home from 'pages/Home';
 import QuizPage from 'pages/QuizPage';
 import ErrorPage from 'pages/ErrorPage';
+import RouteChangeTracker from 'RouteChangeTracker';
+
 const App = () => {
   useEffect(() => {
     if (!window.Kakao.isInitialized()) {
       window.Kakao.init(process.env.REACT_APP_KAKAO);
     }
   }, [window.Kakao]);
+  RouteChangeTracker();
 
   return (
-    <div className="flex relative z-[1] overflow-y-auto  mx-auto my-0 max-w-sm min-h-[667px] h-full">
-      <div className="sm:max-w-xl w-full ">
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />}></Route>
-            <Route path="/result" element={<Result />}></Route>
-            <Route path="/quiz" element={<QuizPage />}></Route>
-            <Route path="/*" element={<ErrorPage />}></Route>
-          </Routes>
-        </BrowserRouter>
-      </div>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/result" element={<Result />}></Route>
+      <Route path="/quiz" element={<QuizPage />}></Route>
+      <Route path="/*" element={<ErrorPage />}></Route>
+    </Routes>
   );
 };
 
