e757253ee6bddbbf4d74344682938e3416511f31
hotdog1004 <hotdog87675210@gmail.com>
2024-01-16 23:49:45 +0900

    Feat : 전체 결과 페이지 추가

---
+++ src/pages/AllResult.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MainLayout from 'layouts/MainLayout';
+
+const AllResult = () => {
+  return (
+    <MainLayout>
+      <h2>전체결과</h2>
+    </MainLayout>
+  );
+};
+
+export default AllResult;
