08abf93ff8e3dfc6aa21a2bebce5c411405362f6
hotdog1004 <hotdog87675210@gmail.com>
2024-01-22 01:50:58 +0900

    Design : CSS 수정
    margin 값 수정, title에 하이라이트 효과 추가

--- src/pages/AllResult.tsx
+++ src/pages/AllResult.tsx
@@ -12,15 +12,15 @@ const AllResult = () => {
     <MainLayout>
       <div className="flex flex-col items-center mb-5">
         <p className="text-xl my-4 mb-1">😺😺😺</p>
-        <p className="text-xl font-semibold"> 전체 유형 보기</p>
+        <p className="text-xl font-semibold">전체 유형 보기</p>
       </div>
       <div className="flex flex-col items-center mb-10">
         {resultList.map((result, index) => (
           <div
             key={index}
-            className="flex items-center border-solid border-slate-900 border-2 rounded-lg w-[95%] h-32 pr-1 m-4"
+            className="flex items-center border-solid border-slate-900 border-2 rounded-lg w-[95%] h-full min-h-48 pr-1 m-4"
           >
-            <div className="w-32 h-24 overflow-hidden">
+            <div className="w-32 h-24 overflow-hidden my-4">
               <img
                 className="w-full h-full object-contain rounded-lg"
                 src={specialCat}
@@ -28,8 +28,14 @@ const AllResult = () => {
               ></img>
             </div>
             <div className="ml-1">
-              <p className="text-sm">{result.subtitle}</p>
-              <p className="text-lg font-semibold">{result.title}</p>
+              <p className="text-sm font-semibold whitespace-pre-line">
+                {result.subtitle}
+              </p>
+              <p className="text-lg font-semibold">
+                <p className="inline bg-gradient-to-b from-transparent from-30% to-amber-300 to-70%">
+                  {result.title}
+                </p>
+              </p>
             </div>
           </div>
         ))}
