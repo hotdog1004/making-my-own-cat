8bccdb8a5f97d2248dcb499ac9dfb370cc4ed08e
hotdog1004 <hotdog87675210@gmail.com>
2024-01-23 16:07:32 +0900

    Design : 문구 수정
    문구와 스타일 수정

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -2,13 +2,13 @@ import Button from 'components/Button';
 import MainLayout from 'layouts/MainLayout';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import specialCat from '../assets/special_cat_3.png';
+import specialCat from '../assets/special_cat.webp';
 const ErrorPage = () => {
   const movePage = useNavigate();
 
   return (
     <MainLayout>
-      <div className="flex flex-col items-center	justify-center h-full">
+      <div className="flex flex-col items-center justify-center h-full">
         <p className="text-xl font-semibold my-4">
           저런! 잘못된 주소로 접근하셨어요!
         </p>
@@ -17,7 +17,9 @@ const ErrorPage = () => {
           src={specialCat}
           alt="고양이이미지"
         ></img>
-        <p className="my-4">들어오신 김에 저희집 고양이 좀 보고가세요 😜😜😜</p>
+        <p className="my-6 mx-2 text-lg whitespace-pre-line text-center	">
+          테스트를 다시 하시면 <br />더 많은 고양이 사진을 볼 수 있어요!😜
+        </p>
       </div>
 
       <div className="flex absolute w-full max-w-sm bottom-0">
